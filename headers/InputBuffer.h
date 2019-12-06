97e73b68e76f29c5e7737f0ceecfc84a90ce5235
James Ferrarelli <jsferrarelli@github.com>
2019-12-05 22:54:00 -0500

    Impletement readLine

--- headers/InputBuffer.h
+++ headers/InputBuffer.h
@@ -1,8 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // fetches the line entered by the user
-void readInput() {
-	
+char *readInput() {
+	int bufferSize = 1024; // arbitrarily picked a size
+	int position = 0;
+	char *buffer = malloc(sizeof(char) * bufferSize); 
+	int c;
+
+	if(!buffer) { // checks if buffer is a null character
+		puts("allocation error: buffer received a null character");
+		exit(EXIT_FAILURE);
+	}	
+
+	// loop through and read the entered line char by char
+	while(1) {
+		c = getchar();
+
+		// when the entire line is read
+		if(c == EOF || c == '\n') { 
+			buffer[position] = '\0';
+			return buffer;
+		}
+		// else put the char in the string
+		else {
+			buffer[position] = c; 
+		}
+		position++;
+
+		// reallocate for when the buffer size is exceeded
+		if(position >= bufferSize) {
+			bufferSize += 1024; // double the size
+			buffer = realloc(buffer, bufferSize);
+		
+			if(!buffer) { // check again for null character
+				puts("allocation error+: buffer received a null character");
+				exit(EXIT_FAILURE);
+			}			
+		}
+	}
 }
 
 // detects the entered commands and arguments
