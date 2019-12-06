0b35550127ce1ada23ece47cf71309529890c58b
James Ferrarelli <jsferrarelli@github.com>
2019-12-05 19:28:39 -0500

    Create terminal.c

---
+++ terminal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define MAXINPUT 1000 // max number of characters supported
+#define MAXLIST 6 // number of commands supported
+
+
+
